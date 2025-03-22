20d0af6d3a9761684d42cdafa54113c9787adc5c
Marcelo Magalhães <celoxd01@gmail.com>
2025-03-22 16:41:05 -0300

    eslintrc - bundle - template = packegesJSON - WEBPACKCONFIG

---
+++ Sections/Section9_JS_Tooling_ES6_Modules/aula93/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path"); // Importar módulo path para resolver caminhos
+
+module.exports = {
+  mode: "production", // Habilitar modo de desenvolvimento
+  entry: "./src/index.js", // Arquivo de entrada
+  output: {
+    path: path.resolve(__dirname, "public", "assets", "js"), // Caminho de saída
+    filename: "bundle.js", // Nome do bundle
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/, // Ignorar a pasta node_modules
+        test: /\.js$/, // Analisar arquivos .js
+        use: {
+          loader: "babel-loader", // Usar Babel para transpilar
+          options: {
+            presets: ["@babel/env"], // Configuração do Babel
+            sourceMaps: true, // Habilitar source maps no Babel
+          },
+        },
+      },
+    ],
+  },
+  devtool: "source-map", // Configuração global para source maps no Webpack
+};
