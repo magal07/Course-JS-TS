35e5e2263bb2c1b755bf06f00f5335ca97d91cf0
Marcelo Magalhães <celoxd01@gmail.com>
2025-03-22 15:18:08 -0300

    Utilizando Babel para "Transconpilar" o arquivo Main.js para o Bundle.js, para melhor acessibilidade dos navegadores

---
+++ Sections/Section9_JS_Tooling_ES6_Modules/aula92/main.js
@@ -0,0 +1,9 @@
+class Person {
+  constructor(name, surname){
+    this.name = name;
+    this.surname = surname;
+  }
+}
+
+//npx babel main.js -o bundle.js --presets=@babel/env
+// bundle é a junção de vários outros arquivos
