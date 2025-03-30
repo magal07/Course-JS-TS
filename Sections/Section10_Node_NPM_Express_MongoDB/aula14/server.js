6ed30e530559269cc6984f4fb8e7e9296a0a9a18
Marcelo Magalhães <celoxd01@gmail.com>
2025-03-30 19:51:22 -0300

    Exemplo aula 14

---
+++ Sections/Section10_Node_NPM_Express_MongoDB/aula14/server.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+/*N precisa pois estamos usando pelo dotenv file, .env 
+const connectionString = 'mongodb+srv://magalhaesdev:zeXKxxFC0OFqm51P@cursojs.u6nwko2.mongodb.net/?retryWrites=true&w=majority&appName=CursoJS';*/
+const mongoose = require('mongoose'); // emitir sinal , base de dados conectada
+mongoose.connect(process.env.CONNECTIONSTRING)
+.then(()=> {
+  console.log('Conectei ao DB');
+  app.emit('Pronto'); // capturando evento
+})
+.catch(e => console.log(e));
+
+const routes = require('./routes');
+const path = require('path');
+const { middewareGlobal } = require('./src/middlewares/middleware');
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static(path.resolve(__dirname, 'public'))); // caminho absoluto de conteudos staticos
+
+app.set('views', path.resolve(__dirname, './src', 'views')); // caminho absoluto da pasta de views
+app.set('view engine', 'ejs'); // usando engine ejs pra conseguir utilizar cods js no html comand terminal (npm i ejs)
+
+// NOSSAS MIDDLEWARES
+app.use(middewareGlobal);
+app.use(routes);
+
+app.on('Pronto', () => { // 'Pronto TEM Q SER IGUAL AO EVENTO EMIT'
+app.listen(3000, () => {
+  console.log('Acessar http://localhost:3000');
+  console.log('Server executando na porta 3000');
+});
+});
+
+
+
+  
---
+++ Sections/Section10_Node_NPM_Express_MongoDB/aula14/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path"); // Importar módulo path para resolver caminhos
+
+module.exports = {
+  mode: "production", // Habilitar modo de desenvolvimento
+  entry: "./frontend/main.js", // Arquivo de entrada
+  output: {
+    path: path.resolve(__dirname, "public", "assets", "js"), // Caminho de saída
+    filename: "bundle.js", // Nome do bundle
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/, // Ignorar a pasta node_modules
+        test: /\.js$/, // Analisar arquivos .js
+        use: {
+          loader: "babel-loader", // Usar Babel para transpilar
+          options: {
+            presets: ["@babel/env"], // Configuração do Babel
+          }
+        }
+      }, {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }]
+  },
+  devtool: "source-map", // Configuração global para source maps no Webpack
+};
