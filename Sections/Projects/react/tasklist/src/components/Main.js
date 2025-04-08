bbd6861f5f984cbf4c0c0e4879af1604831cfdd5
Marcelo Magalhaes <celoxd01@gmail.com>
2025-04-08 17:30:29 -0300

    REACT - Eslint, CREATE Task List

---
+++ Sections/Projects/react/tasklist/src/components/Main.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import './Main.css';
+
+//Form
+import { FaPlus } from 'react-icons/fa';
+// Tasks
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
+
+export default class Main extends Component {
+  state = {
+    newTask: '',
+    tasks: [],
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { tasks } = this.state;
+    let { newTask } = this.state;
+    newTask = newTask.trim();
+
+    if (tasks.indexOf(newTask) !== -1) return;
+
+    const newTasks = [...tasks];
+    this.setState({
+      tasks: [...newTasks, newTask],
+    });
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      newTask: e.target.value,
+    });
+  };
+
+  render() {
+    const { newTask, tasks } = this.state;
+    return (
+      <div className="main">
+        <h1>Task List</h1>
+
+        <form onSubmit={this.handleSubmit} action="#" className="form">
+          <input onChange={this.handleChange} type="text" value={newTask} />
+          <button type="submit">
+            <FaPlus />
+          </button>
+        </form>
+
+        <ul className="tasks">
+          {tasks.map((task) => (
+            <li key={task}>
+              {task}
+              <span>
+                <FaEdit className="edit" />
+                <FaWindowClose className="delete" />
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
