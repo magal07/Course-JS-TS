c9a193326c517e592fd6759f224469d24904dbd7
Marcelo Magalhaes <celoxd01@gmail.com>
2025-04-10 23:42:37 -0300

    Pagination and routes of application

--- Sections/Projects/react/consumerapi/src/components/Header/index.js
+++ Sections/Projects/react/consumerapi/src/components/Header/index.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 
 import { Nav } from './styled';
 
 export default function Header() {
-  const clickedButton = useSelector((state) => state.example.clickedButton);
-
   return (
     <Nav>
       <Link to="/">
         <FaHome size={24} />
       </Link>
-      <Link to="/login">
+      <Link to="/register">
         <FaUserAlt size={24} />
       </Link>
-      <Link to="/outracoisa">
+      <Link to="/login">
         <FaSignInAlt size={24} />
       </Link>
-      {clickedButton ? 'Clicked' : 'Dont Clicked'}
     </Nav>
   );
 }
